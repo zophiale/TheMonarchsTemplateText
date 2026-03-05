250d5d49434859a34cbeed21cb079fe7ae82e534
Zophia H <lestusj@gmail.com>
2026-03-05 02:15:44 +0000

    Main Header

---
+++ MainHeaderV1.cpp
@@ -0,0 +1,35 @@
+/* Welcome to the Main Header, this is the header that is placed at the top of every CPP file I ever write post March 1 2026.
+ * It was made with love and OCD and insanity!
+ * I just love coding lol, but it gets boring, I spice it up with these! */
+//CONTENT BEGINS BELOW
+
+// ==!==!==!==!==!==!== //
+// Zophia Horvath	//
+// FILE			//
+// Created:		//
+// DD/MM/YYYY		//
+// Updated:		//
+// DD/MM/YYYY		//
+// ==!==!==!==!==!==!==	//
+// ==    Contents    == //
+// Lorem Ipsum Ladda L- //
+// -oo what can the ma- //
+// -gic man do for you? //
+// ==   /Contents/   == //
+
+// ==!==!==!==!==!==!== //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
